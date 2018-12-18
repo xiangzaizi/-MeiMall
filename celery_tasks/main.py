32850b9a7f1f7670e9aaca22499c299df4e5ab3f
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-12-18 20:10:43 +0800

    3. 配置celery异步发送短信完成

--- celery_tasks/main.py
+++ celery_tasks/main.py
@@ -2,17 +2,17 @@ from celery import Celery
 # 为celery使用django配置文件进行设置
 import os
 if not os.getenv('DJANGO_SETTINGS_MODULE'):
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'meimall.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'meiMall.settings'
 
 # 创建Celery应用对象
 app = Celery("meiduo")
 
-# 加载celery配置
+# 加载celery配置   config_from_object加载配置的方法
 app.config_from_object('celery_tasks.config')
 
 # 注册异步任务到Celery
 # 自动会从包中读取tasks.py模块中的任务
-app.autodiscover_tasks(["celery_tasks.sms","celery_tasks.email","celery_tasks.html"])
+app.autodiscover_tasks(["celery_tasks.sms"])
 
 # 最终在终端运行这个main文件
 # celery -A 应用包名 worker  -l info
