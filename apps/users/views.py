74a2d6287b640651b1723c59239e50fc000b1297
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-12-18 20:37:15 +0800

    用户模块:用户名和手机号的唯一性校验

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from apps.users.models import User
+
+
+class UserNameCountView(APIView):
+    """
+    用户名数量
+    """
+    # usernames/(?P<username>\w{5,20})/count/
+    def get(self, request, username):
+        """
+        获取指定用户名数量
+        """
+        count = User.objects.filter(username=username).count()
+        data = {
+            'username': username,
+            'count': count
+        }
+        return Response(data)
+
+
+class MobileCountView(APIView):
+    """
+    手机号数量
+    """
+    # mobiles/(?P<mobile>1[345789]\d{9})/count
+    def get(self, request, mobile):
+        """
+        获取指定手机号数量
+        """
+        count = User.objects.filter(mobile=mobile).count()
+        data = {
+            'mobile': mobile,
+            'count': count
+        }
+        return Response(data)
