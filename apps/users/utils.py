c271a520af9dd19b33e73c0b38ecf78a2681552f
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-12-22 11:55:02 +0800

    找回密码, 重写anthenticate 1.登录页面用户认证OK 2.找回密码页面,用户名与手机号都可以进行校验

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.backends import ModelBackend
+import re
+from .models import User
+
 
 def jwt_response_payload_handler(token, user=None, request=None):
     """
@@ -12,3 +16,34 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'user_id': user.id,
         'username': user.username
     }
+
+
+def get_user_by_account(account):
+    """根据手机号或者用户名获取User模型对象
+    :param account: 手机号/账户名
+    :return: User对象/None
+    """
+    try:
+        if re.match('^1[345789]\d{9}$', account):
+            # 账号为手机号
+            user = User.objects.get(mobile=account)
+        else:
+            # 账号为用户名
+            user = User.objects.get(username=account)
+    except User.DoesNotExist:
+        return None
+    else:
+        return user
+
+
+class UsernameMobileAuthBackend(ModelBackend):
+    """
+    自定义用户名或手机号认证
+    """
+    # ()同源代码内容
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        # 新写一个方法兼容手机号和用户名的校验
+        user = get_user_by_account(username)
+        # 判断是否可以通过账号信息获取用户对象,并且密码正确
+        if user is not None and user.check_password(password):
+            return user
